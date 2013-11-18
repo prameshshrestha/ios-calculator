f8b9cf599fba4f54da265cd2bf85a86ab9f237ad
pramesh <pramesh@prameshs-Mac.local>
2013-11-18 09:30:26 -0600

    Initial Commit

---
+++ calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by pramesh on 11/18/13.
+//  Copyright (c) 2013 pramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by pramesh on 11/18/13.
+//  Copyright (c) 2013 pramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
